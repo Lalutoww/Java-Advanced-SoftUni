18f22e15e90f3477afa5f5229dc0404e8a80cf64
Martin Lalutov <martinlalutov00@gmail.com>
2025-02-08 16:06:38 +0200

    [add] GenericBox (String & Integer)

---
+++ 07.Generics/Exercise/src/GenericBox/IntegerDemo.java
@@ -0,0 +1,16 @@
+package GenericBox;
+
+import java.util.Scanner;
+
+public class IntegerDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            int element = Integer.parseInt(sc.nextLine());
+            Box<Integer> box = new Box<>(element);
+            System.out.println(box);
+        }
+    }
+}
---
+++ 07.Generics/Exercise/src/GenericBox/StringDemo.java
@@ -0,0 +1,16 @@
+package GenericBox;
+
+import java.util.Scanner;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            String element = sc.nextLine();
+            Box<String> box = new Box<>(element);
+            System.out.println(box);
+        }
+    }
+}
