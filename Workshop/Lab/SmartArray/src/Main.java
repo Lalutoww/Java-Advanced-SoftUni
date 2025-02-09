98043e5e67ba441ff70d85ff4e13c21cc17d7fce
Martin Lalutov <martinlalutov00@gmail.com>
2025-02-09 23:14:37 +0200

    [add] get and checkIndex methods

--- Workshop/Lab/SmartArray/src/Main.java
+++ Workshop/Lab/SmartArray/src/Main.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         SmartArray smartArray = new SmartArray();
@@ -9,11 +6,10 @@ public class Main {
         smartArray.add(1);
         smartArray.add(2);
         smartArray.add(3);
-
         smartArray.add(4);
+        smartArray.add(5);
 
-        System.out.println();
-
-
+        System.out.println(smartArray.get(4));
+        System.out.println(smartArray.get(5));
     }
 }
--- Workshop/Lab/SmartArray/src/SmartArray.java
+++ Workshop/Lab/SmartArray/src/SmartArray.java
@@ -1,13 +1,17 @@
-import java.util.Collection;
-
 public class SmartArray {
+    /**
+     * Default initial capacity.
+     */
     private static final int DEFAULT_CAPACITY = 4;
+
+    /**
+     * Default initial size.
+     */
     private static final int DEFAULT_SIZE = 0;
 
-
     private int[] data;
-    private int size; //? Holds the real size of the array
-    private int capacity; //? Holds the whole size of the array
+    private int size;
+    private int capacity;
 
     /**
      * Constructs a SmartArray containing the elements of the specified
@@ -15,7 +19,6 @@ public class SmartArray {
      * <p>
      * This SmartArray is based on a normal Array
      */
-
     public SmartArray() {
         this.data = new int[DEFAULT_CAPACITY];
         this.size = DEFAULT_SIZE;
@@ -27,7 +30,6 @@ public class SmartArray {
      *
      * @param element element to be appended to this list
      */
-
     public void add(int element) {
         if (this.size == this.capacity) {
             this.resize();
@@ -37,10 +39,43 @@ public class SmartArray {
     }
 
     /**
+     * Returns the element at the specified position in this list.
+     *
+     * @param index index of the element to return
+     * @return the element at the specified position in this list
+     * @throws IndexOutOfBoundsException – if the index is out of range {@code (index < 0 || index >= size())}
+     */
+    public int get(int index) {
+        this.checkIndex(index);
+
+        return this.data[index];
+    }
+
+    /**
+     * Checks if the {@code index} is within the bounds of the range from
+     * {@code 0} (inclusive) to {@code length} (exclusive).
+     *
+     * <p>The {@code index} is defined to be out of bounds if any of the
+     * following inequalities is true:
+     * <ul>
+     *  <li>{@code index < 0}</li>
+     *  <li>{@code index >= length}</li>
+     * </ul>
+     *
+     * @param index the index
+     * @throws IndexOutOfBoundsException if the {@code index} is out of bounds
+     */
+    private void checkIndex(int index) {
+        if (index < 0 || index >= this.size) {
+            String message = String.format("Index %d is out of bounds.", index);
+            throw new IndexOutOfBoundsException(message);
+        }
+    }
+
+    /**
      * Increases the capacity to ensure that it can hold at least the
      * number of elements specified by the minimum capacity argument.
      */
-
     private void resize() {
         this.capacity *= 2;
         int[] temp = new int[this.capacity];
