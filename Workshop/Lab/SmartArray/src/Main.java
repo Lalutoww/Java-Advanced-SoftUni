867a80bd7e5a8c88c415a56775555c0fac681476
Martin Lalutov <martinlalutov00@gmail.com>
2025-02-09 22:56:21 +0200

    [add]  SmartArray class and add,resize methods

---
+++ Workshop/Lab/SmartArray/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        SmartArray smartArray = new SmartArray();
+
+        smartArray.add(0);
+        smartArray.add(1);
+        smartArray.add(2);
+        smartArray.add(3);
+
+        smartArray.add(4);
+
+        System.out.println();
+
+
+    }
+}
--- Workshop/Lab/SmartArray/src/SmartArray.java
+++ Workshop/Lab/SmartArray/src/SmartArray.java
@@ -1,2 +1,50 @@
+import java.util.Collection;
+
 public class SmartArray {
+    private static final int DEFAULT_CAPACITY = 4;
+    private static final int DEFAULT_SIZE = 0;
+
+
+    private int[] data;
+    private int size; //? Holds the real size of the array
+    private int capacity; //? Holds the whole size of the array
+
+    /**
+     * Constructs a SmartArray containing the elements of the specified
+     * with a default capacity of 4 and default size of 0
+     * <p>
+     * This SmartArray is based on a normal Array
+     */
+
+    public SmartArray() {
+        this.data = new int[DEFAULT_CAPACITY];
+        this.size = DEFAULT_SIZE;
+        this.capacity = DEFAULT_CAPACITY;
+    }
+
+    /**
+     * Appends the specified element to the end of this list.
+     *
+     * @param element element to be appended to this list
+     */
+
+    public void add(int element) {
+        if (this.size == this.capacity) {
+            this.resize();
+        }
+        this.data[size] = element;
+        size++;
+    }
+
+    /**
+     * Increases the capacity to ensure that it can hold at least the
+     * number of elements specified by the minimum capacity argument.
+     */
+
+    private void resize() {
+        this.capacity *= 2;
+        int[] temp = new int[this.capacity];
+        System.arraycopy(this.data, 0, temp, 0, data.length);
+        data = temp;
+    }
 }
