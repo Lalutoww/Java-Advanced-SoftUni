f3d125c4e23e69954e00b79e963993357434c96f
Martin Lalutov <martinlalutov00@gmail.com>
2025-02-10 07:03:58 +0200

    add push method and a resize helper method

--- Workshop/Lab/CustomStack/src/CustomStack.java
+++ Workshop/Lab/CustomStack/src/CustomStack.java
@@ -1,3 +1,40 @@
 public class CustomStack {
-    
+    private static final int DEFAULT_CAPACITY = 4;
+
+    private static final int DEFAULT_SIZE = 0;
+
+    private int[] data;
+    private int size;
+    private int capacity;
+
+    public CustomStack() {
+        this.size = DEFAULT_SIZE;
+        this.capacity = DEFAULT_CAPACITY;
+        this.data = new int[DEFAULT_CAPACITY];
+    }
+
+    /**
+     * Inserts the specified element at the top of the stack.
+     *
+     * @param element the element to add
+     */
+    public void push(int element) {
+        if (size >= capacity) {
+            this.resize();
+        }
+
+        this.data[size] = element;
+        size++;
+    }
+
+    /**
+     * Increases the capacity of this stack by 2 each time {@code size == capacity}.
+     *
+     */
+    private void resize() {
+        this.capacity *= 2;
+        int[] temp = new int[this.capacity];
+        System.arraycopy(this.data, 0, temp, 0, size);
+        this.data = temp;
+    }
 }
---
+++ Workshop/Lab/CustomStack/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayDeque;
+
+public class Main {
+    public static void main(String[] args) {
+        CustomStack customStack = new CustomStack();
+
+        customStack.push(1);
+        customStack.push(2);
+        customStack.push(3);
+
+        System.out.println();
+
+    }
+}
