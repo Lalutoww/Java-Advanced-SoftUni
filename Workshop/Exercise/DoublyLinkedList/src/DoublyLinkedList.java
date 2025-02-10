36a700f522b760f3411172f492bb611d9d75d516
Martin Lalutov <martinlalutov00@gmail.com>
2025-02-10 12:48:57 +0200

    add documentation and a constant for size

--- Workshop/Exercise/DoublyLinkedList/src/DoublyLinkedList.java
+++ Workshop/Exercise/DoublyLinkedList/src/DoublyLinkedList.java
@@ -4,16 +4,26 @@ import java.util.NoSuchElementException;
 import java.util.function.Consumer;
 
 public class DoublyLinkedList<T> {
+    private static final int DEFAULT_SIZE = 0;
+
     private Node<T> head;
     private Node<T> tail;
     private int size;
 
+    /**
+     * Constructs the linkedList by setting the head and tail to null, and sets the size.
+     */
     public DoublyLinkedList() {
         this.head = null;
         this.tail = null;
-        this.size = 0;
+        this.size = DEFAULT_SIZE;
     }
 
+    /**
+     * adds T as first element.
+     *
+     * @param element element to be added.
+     */
     public void addFirst(T element) {
         Node<T> newNode = new Node<>(element);
         if (isEmpty()) {
@@ -27,6 +37,11 @@ public class DoublyLinkedList<T> {
         this.size++;
     }
 
+    /**
+     * Adds T as last element.
+     *
+     * @param element element to be added.
+     */
     public void addLast(T element) {
         Node<T> newNode = new Node<>(element);
         if (isEmpty()) {
@@ -39,6 +54,12 @@ public class DoublyLinkedList<T> {
         this.size++;
     }
 
+    /**
+     * Gets the element at index position.
+     *
+     * @param index position of the element.
+     * @throws IndexOutOfBoundsException if index is out of bound {@link #checkIndex}.
+     */
     public T get(int index) {
         checkIndex(index);
         if (index == 0) {
@@ -64,6 +85,12 @@ public class DoublyLinkedList<T> {
         return currentNode.getValue();
     }
 
+    /**
+     * Removes the head element (first element).
+     *
+     * @return The value of the removed element.
+     * @throws NoSuchElementException if the list is empty.
+     */
     public T removeFirst() {
         if (isEmpty()) {
             throw new NoSuchElementException("The list is empty");
@@ -80,6 +107,12 @@ public class DoublyLinkedList<T> {
         }
     }
 
+    /**
+     * Removes the tail element (last element).
+     *
+     * @return The value of the removed element.
+     * @throws NoSuchElementException if the list is empty.
+     */
     public T removeLast() {
         if (isEmpty()) {
             throw new NoSuchElementException("The list is empty");
@@ -96,6 +129,11 @@ public class DoublyLinkedList<T> {
         }
     }
 
+    /**
+     * Iterates through all elements in the linked list.
+     *
+     * @param consumer lambda function to be executed.
+     */
     public void forEach(Consumer<T> consumer) {
         Node<T> currentNode = this.head;
         while (currentNode != null) {
@@ -104,18 +142,34 @@ public class DoublyLinkedList<T> {
         }
     }
 
+    /**
+     * Puts all the node elements in a normal ArrayList.
+     *
+     * @return ArrayList filled with the nodes.
+     */
     public List<T> toList() {
         List<T> nodeList = new ArrayList<>();
         forEach(nodeList::add);
         return nodeList;
     }
 
+    /**
+     * Checks if the index is in bounds.
+     *
+     * @param index the index to be checked.
+     * @throws IndexOutOfBoundsException if the index is not present in the linked list.
+     */
     private void checkIndex(int index) {
         if (index < 0 || index >= this.size) {
             throw new IndexOutOfBoundsException(String.format("Index %d is out of bounds.", index));
         }
     }
 
+    /**
+     * Checks if the index is in bounds.
+     *
+     * @return boolean value -> {@code true} if is empty, and {@code false} otherwise.
+     */
     private boolean isEmpty() {
         return this.size == 0;
     }
